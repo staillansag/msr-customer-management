f50a591956530f2b747943b31b71ee2bfed63095
staillansag <stt.lab@outlook.com>
2022-12-02 16:58:59 +0100

    .

--- buildScripts/04.build.sh
+++ buildScripts/04.build.sh
@@ -19,13 +19,17 @@ echo "Dumping environment for demo purposes"
 env | sort
 
 echo "Logging in to repository ${MY_AZ_ACR_URL}"
-buildah login -u "${AZ_ACR_SP_ID}" -p "${AZ_ACR_SP_SECRET}" "${MY_AZ_ACR_URL}"  || exit 3
+#buildah login -u "${AZ_ACR_SP_ID}" -p "${AZ_ACR_SP_SECRET}" "${MY_AZ_ACR_URL}"  || exit 3
 sudo docker login -u "${AZ_ACR_SP_ID}" -p "${AZ_ACR_SP_SECRET}" "${MY_AZ_ACR_URL}"  || exit 3
 
 echo "Building tag ${OUR_SERVICE_TAG_BASE}"
-buildah bud \
+#buildah bud \
+#  --build-arg __from_img=${AZ_BASE_IMAGE_TAG} \
+#  --format docker \
+#  -t "${OUR_SERVICE_TAG_BASE}" || exit 4
+
+docker build \
   --build-arg __from_img=${AZ_BASE_IMAGE_TAG} \
-  --format docker \
   -t "${OUR_SERVICE_TAG_BASE}" || exit 4
 
 crtTag="${OUR_SERVICE_TAG_BASE}:${OUR_SERVICE_MAJOR_VERSION}.${OUR_SERVICE_MINOR_VERSION}.${BUILD_BUILDID}"
@@ -44,7 +48,7 @@ if [[ "${BUILD_SOURCEBRANCHNAME}" == "main" ]]; then
 fi
 
 echo "Logging out"
-buildah logout "${MY_AZ_ACR_URL}"
+#buildah logout "${MY_AZ_ACR_URL}"
 sudo docker logout "${MY_AZ_ACR_URL}"
 
 echo "Push completed"
