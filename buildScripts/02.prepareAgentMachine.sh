22da4d73e9fefc5d89a9452e924aa0320e408e60
staillansag <stt.lab@outlook.com>
2022-12-03 19:10:25 +0100

    .

--- buildScripts/02.prepareAgentMachine.sh
+++ buildScripts/02.prepareAgentMachine.sh
@@ -3,9 +3,7 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 
 echo "kubectl install"
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check || exit 1
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" > /dev/null
 
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl || exit 1
 
@@ -14,6 +12,5 @@ kubectl version --client || exit 1
 mkdir ~/.kube
 cp "${KUBECTLCONFIG_SECUREFILEPATH}" ~/.kube/config
 
-kubectl get nodes
+kubectl get nodes || exit 1
 
-find ~/ -ls
