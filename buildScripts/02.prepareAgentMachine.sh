1e8566033e3bb69db4e0efc4cbad9163515513f3
staillansag <stt.lab@outlook.com>
2022-12-03 20:59:52 +0100

    .

--- buildScripts/02.prepareAgentMachine.sh
+++ buildScripts/02.prepareAgentMachine.sh
@@ -14,3 +14,10 @@ cp "${KUBECTLCONFIG_SECUREFILEPATH}" ~/.kube/config
 
 kubectl get nodes || exit 1
 
+echo "Creating K8S secret to connect to the SAG Docker registry"
+kubectl create secret docker-registry sagregcred --docker-server=sagcr.azurecr.io --docker-username="${SAG_ACR_SP_ID}" --docker-password="${SAG_ACR_SP_SECRET}" --docker-email="${MY_EMAIL_ADDRESS}"
+
+echo "Creating K8S secret to connect to the stt Docker registry"
+kubectl create secret docker-registry sttregcred --docker-server="${MY_AZ_ACR_URL}" --docker-username="${AZ_ACR_SP_ID}" --docker-password="${AZ_ACR_SP_SECRET}" --docker-email="${MY_EMAIL_ADDRES}"
+
+kubectl apply -f "${KUBERNETESSECRETS_SECUREFILEPATH}"
