c100e0827a19bfd1b383b898c82e27094e3c132a
staillansag <stt.lab@outlook.com>
2022-12-03 21:41:50 +0100

    .

--- buildScripts/02.prepareAgentMachine.sh
+++ buildScripts/02.prepareAgentMachine.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 . ./buildScripts/setEnv.sh
 
-env
+echo "Getting service principal credentials..."
+if [ ! -f "${ACRSPCREDENTIALS_SECUREFILEPATH}" ]; then
+  echo "Secure file path not present: ${ACRSPCREDENTIALS_SECUREFILEPATH}"
+  exit 1
+fi
+
+chmod u+x "${ACRSPCREDENTIALS_SECUREFILEPATH}"
+. "${ACRSPCREDENTIALS_SECUREFILEPATH}"
 
 echo "Docker install"
 curl -fsSL https://get.docker.com -o get-docker.sh
@@ -14,6 +21,12 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl || exit 1
 
 kubectl version --client || exit 1
 
+echo "Getting kubectl config..."
+if [ ! -f "${KUBECTLCONFIG_SECUREFILEPATH}" ]; then
+  echo "Secure file path not present: ${KUBECTLCONFIG_SECUREFILEPATH}"
+  exit 1
+fi
+
 mkdir ~/.kube
 cp "${KUBECTLCONFIG_SECUREFILEPATH}" ~/.kube/config
 
@@ -24,5 +37,11 @@ kubectl create secret docker-registry sagregcred --docker-server=sagcr.azurecr.i
 
 echo "Creating K8S secret to connect to the stt Docker registry"
 kubectl create secret docker-registry sttregcred --docker-server="${MY_AZ_ACR_URL}" --docker-username="${AZ_ACR_SP_ID}" --docker-password="${AZ_ACR_SP_SECRET}" --docker-email="${MY_EMAIL_ADDRES}"
+
+echo "Getting kubernetes secrets..."
+if [ ! -f "${KUBERNETESSECRETS_SECUREFILEPATH}" ]; then
+  echo "Secure file path not present: ${KUBERNETESSECRETS_SECUREFILEPATH}"
+  exit 1
+fi
 
 kubectl apply -f "${KUBERNETESSECRETS_SECUREFILEPATH}"
