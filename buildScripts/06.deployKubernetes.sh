d55b11234ff500ef8c0b7dbc605542b872eb5583
staillansag <stt.lab@outlook.com>
2022-12-04 15:36:31 +0100

    .

--- buildScripts/06.deployKubernetes.sh
+++ buildScripts/06.deployKubernetes.sh
@@ -1,10 +1,10 @@
  #!/bin/bash
 . ./buildScripts/setEnv.sh
 
-crtTag="${OUR_SERVICE_TAG_BASE}:${OUR_SERVICE_MAJOR_VERSION}.${OUR_SERVICE_MINOR_VERSION}.${BUILD_BUILDID}"
+imageTag="${OUR_SERVICE_MAJOR_VERSION}.${OUR_SERVICE_MINOR_VERSION}.${BUILD_BUILDID}"
 
 echo "Deploying new microservice image"
-sed 's/customer\-management\:latest/customer\-management\:${crtTag}/g' ./deployment/kubernetes/01_msr-customer-management_deployment.yaml | kubectl apply -f - || exit 6
+sed 's/customer\-management\:latest/customer\-management\:'${imageTag}'/g' ./deployment/kubernetes/01_msr-customer-management_deployment.yaml | kubectl apply -f - || exit 6
 
 echo "Deploying service (in case it does not already exist)"
 kubectl apply -f ./deployment/kubernetes/02_msr-customer-management_service.yaml || exit 6
