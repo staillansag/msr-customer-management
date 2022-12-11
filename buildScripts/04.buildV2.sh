07bba906a6e227d75c1ab6b75e5934412b8f9d78
staillansag <stt.lab@outlook.com>
2022-12-11 12:05:05 +0100

    .

--- buildScripts/04.buildV2.sh
+++ buildScripts/04.buildV2.sh
@@ -29,7 +29,7 @@ docker build \
 dockerHostName=$(cat /dev/urandom | tr -dc '[:alpha:]' | fold -w ${1:-20} | head -n 1 | tr '[:upper:]' '[:lower:]')
 
 echo "Environment file for testing: ${DOCKERENV_SECUREFILEPATH}"
-dockerId=$(docker run --name ${nameSuffix} -dp 5555:5555 -d --network sag --env-file ${DOCKERENV_SECUREFILEPATH} "${OUR_SERVICE_TAG_BASE}")
+dockerId=$(docker run --name ${dockerHostName} -dp 5555:5555 -d --network sag --env-file ${DOCKERENV_SECUREFILEPATH} "${OUR_SERVICE_TAG_BASE}")
 
 echo "Checking availability of http://${dockerHostName}:5555"
 max_retry=10
