f8c395c9d3b1eab6938b2fd48a4bce4e0fc26565
staillansag <stt.lab@outlook.com>
2022-12-22 14:48:30 +0100

    Adding new K8S deployment topology

---
+++ buildScripts/06.deployKubernetesV2.sh
@@ -0,0 +1,29 @@
+ #!/bin/bash
+. ./buildScripts/setEnv.sh
+
+echo "Getting the ID of the current revision, in case we have to rollback to it"
+rollbackRevision=$(kubectl rollout history deployment/customer-management -o jsonpath='{.metadata.generation}')
+echo "##vso[task.setvariable variable=ROLLBACK_REVISION;]${rollbackRevision}"
+
+imageTag="${OUR_SERVICE_MAJOR_VERSION}.${OUR_SERVICE_MINOR_VERSION}.${BUILD_BUILDID}"
+
+echo "Deploying new msr image"
+sed 's/customer\-management\:latest/customer\-management\:'${imageTag}'/g' ./deployment/kubernetes/01_msr-customer-management_deployment.yaml | kubectl apply -f - || exit 6
+
+echo "Deploying msr service (in case it does not already exist)"
+kubectl apply -f ./deployment/kubernetes/02_msr-customer-management_service.yaml || exit 6
+
+echo "Deploying microgateway image"
+kubectl apply -f ./deployment/kubernetes/03_mcgw-customer-management_service.yaml || exit 6
+
+echo "Deploying microgateway service"
+kubectl apply -f ./deployment/kubernetes/04_mcgw-customer-management_service.yaml || exit 6
+
+echo "Deploying ingress"
+kubectl apply -f ./deployment/kubernetes/99_ingress.yaml || exit 6
+
+echo "Waiting for deployment to complete"
+kubectl rollout status deployment customer-management --timeout=300s && echo "Deployment complete" || exit 6
+
+echo "Checking service readiness (by making a call to the microgateway metrics endpoint)"
+curl -s -o /dev/null --location --request GET "https://customers.sttlab.eu/metrics" && echo "Service is up" || exit 6
