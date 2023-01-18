64b1e7f2e31c26109a1b89c5d71a84c145bf9add
staillansag <stt.lab@outlook.com>
2023-01-18 08:54:11 +0100

    Adding option to skip microgateway deployment

--- buildScripts/06.deployKubernetesV2.sh
+++ buildScripts/06.deployKubernetesV2.sh
@@ -13,14 +13,22 @@ sed 's/customer\-management\:latest/customer\-management\:'${imageTag}'/g' ./dep
 echo "Deploying msr service (in case it does not already exist)"
 kubectl apply -f ./deployment/kubernetes/02_msr-customer-management_service.yaml || exit 6
 
-echo "Deploying microgateway image"
-kubectl apply -f ./deployment/kubernetes/03_mcgw-customer-management_deployment.yaml || exit 6
+echo "Microgateway deployment skipped: ${SKIP_MICROGATEWAY}"
 
-echo "Deploying microgateway service"
-kubectl apply -f ./deployment/kubernetes/04_mcgw-customer-management_service.yaml || exit 6
+if [ "${SKIP_MICROGATEWAY}" = "true" ]
+then
+    # If microgateway deployment is skipped, we make the ingress controller point to the msr service
+    sed 's/mcgw\-customer\-management/msr\-customer\-management/g' ./deployment/kubernetes/99_ingress.yaml | kubectl apply -f - || exit 6
+else
+    echo "Deploying microgateway image"
+    kubectl apply -f ./deployment/kubernetes/03_mcgw-customer-management_deployment.yaml || exit 6
 
-echo "Deploying ingress"
-kubectl apply -f ./deployment/kubernetes/99_ingress.yaml || exit 6
+    echo "Deploying microgateway service"
+    kubectl apply -f ./deployment/kubernetes/04_mcgw-customer-management_service.yaml || exit 6
+
+    echo "Deploying ingress"
+    kubectl apply -f ./deployment/kubernetes/99_ingress.yaml || exit 6
+fi
 
 echo "Waiting for deployment to complete"
 kubectl rollout status deployment customer-management --timeout=300s && echo "Deployment complete" || exit 6
